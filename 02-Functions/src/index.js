354c809c5665746dd1bc03ce8be4e029c92862f6
Shelby Talbert <shelby.talbert@flatironschool.com>
2022-11-03 11:42:59 -0400

    ready for lecture 2

---
+++ 02-Functions/src/index.js
@@ -0,0 +1,12 @@
+ //1. Create a constant variable called bookstore with the value 'Easley\'s Technical Books' (or "Easley's Technical Books")
+
+//2. Create a variable that can be reassigned called open with the value false
+
+
+//3. Create a conditional message based on the value of open. If open is true log "Welcome to {bookstore}, we're open" else "Sorry, {bookstore} is closed"
+
+
+//4. Create a function that toggles the value of open between T and F.
+
+
+
