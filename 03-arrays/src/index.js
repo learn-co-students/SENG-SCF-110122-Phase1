9f2d6d8069851e6f6207602d2fbf312b89ad45f4
Shelby Talbert <shelby.talbert@flatironschool.com>
2022-11-04 12:25:12 -0400

    ready for lecture 3

---
+++ 03-arrays/src/index.js
@@ -0,0 +1,16 @@
+// 1. Store the books and customer names in arrays. 
+
+
+// 2. Write a functions that can transform customers elements. 
+
+// - Change books to uppercase
+// - Remove the author
+// - Remove the title
+
+
+// 3. Write a function that will filter books out by a search term.
+
+
+// 4. Write a function that returns a specific book by search term. 
+
+
