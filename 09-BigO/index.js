1a52b0d9776171cba245d88ea13e472e0c4ba324
Shelby Talbert <shelby.talbert@flatironschool.com>
2022-11-30 19:00:23 -0500

    need indexhtml

---
+++ 09-BigO/index.js
@@ -0,0 +1,10 @@
+console.log("Hi - Welcome to Big O")
+
+
+function findEl(el, arr) {
+  // return index of the el in the array 
+  // if index is not in array, return -1
+}
+
+// worst case scenario
+// findEl(4, [1,2,3,4])
